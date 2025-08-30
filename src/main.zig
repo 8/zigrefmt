73375224c77e16cdf6a2abe693c3634a2163d569
mdk <mdk@lostindetails.com>
2025-08-30 20:10:31 +0200

    creates formatted backup files

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const postfmt = @import("postfmt");
 
-const Error = postfmt.FormatError ||
-                    std.process.Child.RunError ||
-                    std.fs.File.OpenError ||
-                    std.fs.Dir.StatError ||
-                    std.io.Reader.DelimiterError ||
-                    error { ZigFormatError, ArgError };
+const Error =
+  postfmt.FormatError ||
+  std.process.Child.RunError ||
+  std.fs.File.OpenError ||
+  std.fs.Dir.StatError ||
+  std.io.Reader.DelimiterError ||
+  std.io.Writer.Error ||
+  error { ZigFormatError, ArgError };
 
 pub fn main() !void {
 
@@ -23,7 +25,8 @@ pub fn main() !void {
 
     // then fix the identation
     const dir = std.fs.cwd();
-    try fixIndentationForPath(dir, file);
+    const format = postfmt.FormatOptions.toSpaces(2);
+    try fixIndentationForPath(dir, file, format);
 
   } else {
     std.debug.print("error: expected at least one source file argument\n", .{});
@@ -72,7 +75,11 @@ fn execZigFmt(allocator: std.mem.Allocator, file: []const u8) Error!void {
   // });
 }
 
-fn fixIndentationForPath(dir: std.fs.Dir, path: []const u8) Error!void {
+fn isZigFile(file: []const u8) bool {
+  return std.mem.eql(u8, ".zig", std.fs.path.extension(file));
+}
+
+fn fixIndentationForPath(dir: std.fs.Dir, path: []const u8, format: postfmt.FormatOptions) Error!void {
 
   const file_or_directory = try dir.openFile(path, .{});
   defer file_or_directory.close();
@@ -80,20 +87,20 @@ fn fixIndentationForPath(dir: std.fs.Dir, path: []const u8) Error!void {
   const stat = try file_or_directory.stat();
 
   return switch (stat.kind) {
-    .directory => fixIndentationForDirectory(std.fs.Dir { .fd = file_or_directory.handle } ),
-    .file => fixIndentationForFile(file_or_directory),
+    .directory => fixIndentationForDirectory(std.fs.Dir { .fd = file_or_directory.handle }, format),
+    .file => if (isZigFile(path)) { try fixIndentationForFile(file_or_directory, path, dir, format); },
     else => {},
   };
 }
 
-fn fixIndentationForDirectory(directory: std.fs.Dir) Error!void {
+fn fixIndentationForDirectory(directory: std.fs.Dir, format: postfmt.FormatOptions) Error!void {
 
   // std.debug.print("directory\n", .{});
   var iterator = directory.iterate();
   while (iterator.next()) |maybe_entry| {
     if (maybe_entry) |entry| {
       // std.debug.print("{s}\n", .{entry.name});
-      try fixIndentationForPath(directory, entry.name);
+      try fixIndentationForPath(directory, entry.name, format);
     } else {
       return;
     }
@@ -102,22 +109,35 @@ fn fixIndentationForDirectory(directory: std.fs.Dir) Error!void {
   }
 }
 
-fn fixIndentationForFile(input_file: std.fs.File) std.io.Reader.DelimiterError!void {
+fn fixIndentationForFile(input_file: std.fs.File, name: []const u8, dir: std.fs.Dir, format: postfmt.FormatOptions) Error!void {
 
-  var line_read_buffer: [1024*8] u8 = undefined;
+  const line_buf_len = 1024 * 8;
+
+  var line_read_buffer: [line_buf_len] u8 = undefined;
   var reader = input_file.reader(&line_read_buffer);
 
-  // create a new temp file for writing
-  // std.fs.createFileAbsolute(absolute_path: []const u8, flags: CreateFlags)
-  // const 
-  // std.zip.
-  // dir.createFile(, flags: CreateFlags)
+  // create a new temp file for writing in the same directory
+  var temp_file_name_buf: [1024]u8 = undefined;
+  const temp_file_name = try std.fmt.bufPrint(&temp_file_name_buf, ".{s}.bak", .{ name });
+
+  std.debug.print("creating temp file: '{s}'\n", .{temp_file_name});
+
+  const temp_file = try dir.createFile(temp_file_name, .{.lock = .exclusive });
+  defer temp_file.close();
+
+  var line_write_buffer: [line_buf_len] u8 = undefined;
+  var writer = temp_file.writer(&line_write_buffer);
+
+  var format_line_buffer: [line_buf_len] u8 = undefined;
 
   // read the file line by line
   while (reader.interface.takeDelimiterInclusive('\n')) |line| {
-    std.debug.print("{s}", .{line});
+    // std.debug.print("{s}", .{line});
+    const formatted_line = try postfmt.formatLine(line, &format_line_buffer, format);
+    try writer.interface.writeAll(formatted_line);
   } else |err|{
     if (err != error.EndOfStream)
       return err;
   }
+  try writer.interface.flush();
 }
